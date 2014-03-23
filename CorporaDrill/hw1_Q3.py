d2e9ffd5cd52036fcb2661dda1514a5e7cd417da
haimshalev <haimshalev@gmail.com>
2014-03-23 23:04:38 +0200

    Updated the code to create a freq list of the actual data

--- CorporaDrill/hw1_Q3.py
+++ CorporaDrill/hw1_Q3.py
@@ -1,15 +1,26 @@
-# This is only a test
-# A seccessfull one 
-# need to run this on the actual data
+import sys, os, codecs
 
-from collections import Counter
-words = "apple banana apple strawberry banana banana lemon"
-freqs = Counter(words.split())
-print(freqs)
+#get the command line argument
+if len(sys.argv) < 2: sys.exit("Please enter a data directory path")
+currentDir = sys.argv[1]
+print("The data folder is: " + currentDir)
 
-sorted_freqs = freqs.most_common()
-print(sorted_freqs)
+# get all the txt file paths from the given data directory
+txtFilesList = [ os.path.join(currentDir, f) for f in os.listdir(currentDir) if (os.path.isfile(os.path.join(currentDir, f)) & str(f).endswith(".out.txt"))]
 
-import codecs
-outputFileStream = codecs.open("freqlist.txt" , "w", "utf-8")
-outputFileStream.writelines(("%s\r\n" % (str(idx + 1) + " " + val[0] + " " + str(val[1])) for idx, val in enumerate(sorted_freqs)))
+# seperate each text to sentences 
+for f in txtFilesList:
+    print("Creating a frequency list of file :  " + f)
+    
+    # open the input file and create an output file
+    inputFileStream  = codecs.open(f,"r","utf-8")
+    outputFileStream = codecs.open(f[0:(f.rfind(".txt"))] + "_freq.txt" , "w", "utf-8")
+    
+    # class for creating a counted dictionary
+    from collections import Counter
+    
+    # create a dictionary of words sorted from the most common one to the last common one
+    freqs = Counter(inputFileStream.read().split()).most_common()
+    
+    # output the freqs table to the output file
+    outputFileStream.writelines(("%6d|%20s|%6d \r\n" % (idx + 1, val[0], val[1]) for idx, val in enumerate(freqs)))
