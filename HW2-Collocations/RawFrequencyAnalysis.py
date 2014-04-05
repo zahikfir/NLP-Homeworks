cc1a82396ce3dc4b52fb140454c45c5b0a0320cb
unknown <zkk12345@gmail.com>
2014-04-05 18:33:17 +0300

    HW2: Output top 100 Raw Frequency Analysis into RawFrequency_raw.txt file

--- HW2-Collocations/RawFrequencyAnalysis.py
+++ HW2-Collocations/RawFrequencyAnalysis.py
@@ -32,7 +32,16 @@ def RawFrequencyAnalysis(txtFilesList,RF_Raw_outStream,RF_Select_outStream):
             for Itr in range(0,len(TokenList)-1):
                 Collocations_CountedDictionary.update( [ TokenList[Itr] + " " + TokenList[Itr+1] ])
    
-    # Sort the dictionary
-    Collocations_CountedDictionary = Collocations_CountedDictionary.most_common()
+    # Get the top 100 collocations
+    Top100Collocations = Collocations_CountedDictionary.most_common(100)
+
+    # Output the top 100 Raw Frequency Analysis into RawFrequency_raw.txt file
+    RF_Raw_outStream.writelines(("%15d\t%30s\t%.5f \r\n" % (idx + 1, val[0], val[1]*1000/len(Collocations_CountedDictionary) ) for idx, val in enumerate(Top100Collocations)))
+    RF_Raw_outStream.close() 
+    
+
+    # TODO
+    # .most_common(100) is random when equally appear  (sort it Alpha-Beth)
+    # when output the file a letter and a Punctuation, it's appear backwards 
 
     return 'Done'
