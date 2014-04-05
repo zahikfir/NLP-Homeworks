fb4b3a648a26960697045327eec65182f5fd90d2
unknown <zkk12345@gmail.com>
2014-04-05 17:12:07 +0300

    HW2: RawFrequencyAnalysis.py 	Add all the sequential collocations to a counted dictionary 	tested successfully with Notepad++ 'Find all'
    Signed-off-by: unknown <zkk12345@gmail.com>

--- HW2-Collocations/RawFrequencyAnalysis.py
+++ HW2-Collocations/RawFrequencyAnalysis.py
@@ -9,24 +9,30 @@ def RawFrequencyAnalysis(txtFilesList,RF_Raw_outStream,RF_Select_outStream):
     # Analysis each text  
     for f in txtFilesList:
         print("Raw Frequency analysis " + f + " :")
+
+        # Creating a counted dictionary
+        from collections import Counter
+        Collocations_CountedDictionary = Counter()
         
         # open the input file
         inputFileStream  = codecs.open(f,"r","utf-8")
         inputFile = inputFileStream.read()
-        
+        inputFile = inputFile.lower()       # when using english we want "Open minded" to be equal to "open minded"
+
         # Split the text to sentences, (No collocation crosses two sentences)
         f_Sentences = inputFile.split('\r\n')
         
-        for Sentence in f_Sentences:
-            TokenList = Sentence.split()
+        # Analyze each sentence separately 
+        for Sentence in f_Sentences: 
+            
+            # Split the sentence into tokens
+            TokenList = Sentence.split()    
 
-        #for Sentence in f_Sentences:
-        #    RF_Raw_outStream.write(Sentence)
-        #    RF_Raw_outStream.write("\r\n")
-
-        #RF_Raw_outStream.write("*********************************************************************")
-        #RF_Raw_outStream.write("\r\n")
-
-    RF_Raw_outStream.close();
+            # Add all the sequential collocations to the dictionary 
+            for Itr in range(0,len(TokenList)-1):
+                Collocations_CountedDictionary.update( [ TokenList[Itr] + " " + TokenList[Itr+1] ])
+   
+    # Sort the dictionary
+    Collocations_CountedDictionary = Collocations_CountedDictionary.most_common()
 
     return 'Done'
