2d11a078f4aee3bd0c25f555a6b051b72930b051
unknown <zkk12345@gmail.com>
2014-04-05 08:15:12 +0300

    HW2: RawFrequencyAnalysis 	split the text into sentences, 	split the sentences into words.
    Signed-off-by: unknown <zkk12345@gmail.com>

--- HW2-Collocations/RawFrequencyAnalysis.py
+++ HW2-Collocations/RawFrequencyAnalysis.py
@@ -9,8 +9,24 @@ def RawFrequencyAnalysis(txtFilesList,RF_Raw_outStream,RF_Select_outStream):
     # Analysis each text  
     for f in txtFilesList:
         print("Raw Frequency analysis " + f + " :")
-    
-    # open the input file
-    inputFileStream  = codecs.open(f,"r","utf-8")
-    
+        
+        # open the input file
+        inputFileStream  = codecs.open(f,"r","utf-8")
+        inputFile = inputFileStream.read()
+        
+        # Split the text to sentences, (No collocation crosses two sentences)
+        f_Sentences = inputFile.split('\r\n')
+        
+        for Sentence in f_Sentences:
+            TokenList = Sentence.split()
+
+        #for Sentence in f_Sentences:
+        #    RF_Raw_outStream.write(Sentence)
+        #    RF_Raw_outStream.write("\r\n")
+
+        #RF_Raw_outStream.write("*********************************************************************")
+        #RF_Raw_outStream.write("\r\n")
+
+    RF_Raw_outStream.close();
+
     return 'Done'
