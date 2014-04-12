de6db2906047ee1767e4c58f2adb44397f406ba8
unknown <zkk12345@gmail.com>
2014-04-12 09:55:02 +0300

    HW2: RawFrequencyAnalysis() sort the result alphabetically

--- HW2-Collocations/RawFrequencyAnalysis.py
+++ HW2-Collocations/RawFrequencyAnalysis.py
@@ -6,18 +6,26 @@ import sys, os, codecs, re, time, math, operator
 #        RfTop100_OFile / Rf20Appearances_OFile - output files
 def RawFrequencyAnalysis(RfTop100_OFile,Rf20Appearances_OFile,collocationsFreqs,tokensFreqs):
     StartTime = time.clock()
-   
+
     # get the number of tokens in all the corpus
     NumOfTokens = sum(tokensFreqs.values())
+   
+    # Sort the counted dictionary alphabetically
+    collocationsFreqs = sorted(collocationsFreqs.items(),key=operator.itemgetter(0))
+    tokensFreqs = sorted(tokensFreqs.items(),key=operator.itemgetter(0))
+
+    # Sort the Counted dictinary by count
+    collocationsFreqs = sorted(collocationsFreqs, key=lambda tup: tup[1],reverse=True)
+    tokensFreqs = sorted(tokensFreqs, key=lambda tup: tup[1],reverse=True)
 
     # Output the top 100 Raw Frequency scores into RawFrequency_raw.txt file
-    Top100Collocations = collocationsFreqs.most_common(100)    # Get the top 100 collocations
+    Top100Collocations = collocationsFreqs[0:100]    # Get the top 100 collocations
     RfTop100_OFile.writelines((("%15d\t%30s\t%f " + os.linesep) % (idx + 1, val[0], val[1]*1000/NumOfTokens ) for idx, val in enumerate(Top100Collocations)))
     RfTop100_OFile.close() 
     
     # List the collocations with exactly 20 appearances
     TwentyAppearances = []
-    for Key, Value in collocationsFreqs.items():
+    for (Key,Value) in collocationsFreqs:
         if Value == 20:
             TwentyAppearances.append((Key,Value))
 
