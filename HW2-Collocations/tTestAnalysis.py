9bd6db3082914510c9988ee41050ed5fd4ae4684
unknown <zkk12345@gmail.com>
2014-04-12 11:40:06 +0300

    HW2: Sort tTestAnalysis() output alphabetically
    Signed-off-by: unknown <zkk12345@gmail.com>

--- HW2-Collocations/tTestAnalysis.py
+++ HW2-Collocations/tTestAnalysis.py
@@ -6,9 +6,8 @@ import sys, os, codecs, re, time, math, operator
 #        tTestTop100_OFile / tTest20Appearances_OFile - output files
 def tTestAnalysis(tTestTop100_OFile,tTest20Appearances_OFile,collocationsFreqs,tokensFreqs):
     StartTime = time.clock()
-  
-    # get the number of tokens in all the corpus
-    numOfTokens = sum(tokensFreqs.values())
+
+    numOfTokens = sum(tokensFreqs.values())  # Total number of tokens in all the texts
 
     # for each colocation - give it a t-score and put it in a dictionary
     tScoreDic = dict()
@@ -27,8 +26,10 @@ def tTestAnalysis(tTestTop100_OFile,tTest20Appearances_OFile,collocationsFreqs,t
         # set the tscore
         if not sampleVariance == 0 : tScoreDic[collocation] = (math.fabs(sampleVariance - meanOfDistributation)) / (math.sqrt(sampleVariance / numOfTokens))
 
-    # Get the top 100 collocations
-    Top100Collocations = sorted(tScoreDic.items(), key=operator.itemgetter(1),reverse=True)[0:100]
+    # List Top 100 t-test scores
+    Top100Collocations = sorted(tScoreDic.items(),key=operator.itemgetter(0))               # Alphabetically sort
+    Top100Collocations = sorted(Top100Collocations, key=lambda tup: tup[1],reverse=True)    # Sort by count
+    Top100Collocations = Top100Collocations[0:100]                                          # Get only top 100 collocations
 
     # Output the top 100 tTest scores into tTest_raw.txt file
     tTestTop100_OFile.writelines((("%15d\t%30s\t%f " + os.linesep) % (idx + 1, val[0], val[1] ) for idx, val in enumerate(Top100Collocations)))
@@ -39,9 +40,8 @@ def tTestAnalysis(tTestTop100_OFile,tTest20Appearances_OFile,collocationsFreqs,t
     for Key, Value in collocationsFreqs.items():
         if Value == 20:
             TwentyAppearances.append((Key,tScoreDic[Key]))
-
-    # Sort the collocations with exactly 20 appearances by the T-test score
-    TwentyAppearances.sort(key=operator.itemgetter(1),reverse= True)
+    TwentyAppearances.sort(key=operator.itemgetter(0))                  # Alphabetically sort
+    TwentyAppearances.sort(key=operator.itemgetter(1),reverse= True)    # Sort by count
 
     # Output the collocations with exactly 20 appearances with their tTest score into tTest_select.txt file
     tTest20Appearances_OFile.writelines((("%15d\t%30s\t%f " + os.linesep) % (idx + 1, val[0], val[1] ) for idx, val in enumerate(TwentyAppearances)))
