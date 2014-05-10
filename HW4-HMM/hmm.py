c1c00d47c35465225fda2e5f1951894f873a5631
unknown <zkk12345@gmail.com>
2014-05-10 18:58:24 +0300

    HW4 - open empty project HW4-HMM.pyproj

---
+++ HW4-HMM/hmm.py
@@ -0,0 +1,13 @@
+#********************** NLP **********************#
+# H.W 4
+# Haim Shalelashvily    200832780
+# Zahi Kfir             200681476
+#********************** NLP **********************#
+
+import sys, os, time, codecs, math
+from collections import Counter
+
+
+
+
+print("\nHW4 - Hidden Markov model\n") 
