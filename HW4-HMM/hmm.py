658568b44b90cb65895953ccdbabf4a221e867a8
unknown <zkk12345@gmail.com>
2014-05-11 20:44:45 +0300

    HW4: GetCommandLineArguments() 	returns: executionMode , trainFilePath , evalOrTestFilePath
    Signed-off-by: unknown <zkk12345@gmail.com>

--- HW4-HMM/hmm.py
+++ HW4-HMM/hmm.py
@@ -8,6 +8,32 @@ import sys, os, time, codecs, math
 from collections import Counter
 
 
+# Return the execution mod and the appropriate command arguments
+def GetCommandLineArguments():
+    try:
+        if len(sys.argv) < 4: 
+            sys.exit()
+
+        executionMode = sys.argv[1]
+        trainFilePath = sys.argv[sys.argv.index("--train") + 1]
+
+        if (executionMode == '-v'):         # Evaluation mode
+            evalOrTestFilePath = sys.argv[sys.argv.index("--eval") + 1]
+        elif (executionMode == '-t'):       # Test mode 
+            evalOrTestFilePath = sys.argv[sys.argv.index("--test") + 1]
+        else:                               # wrong execution mode
+            sys.exit()
+    
+    except:                                 # if one of the option parameters is missing
+        sys.exit("\nWrong input. Please check your command line arguments \nTo run hmm.py in evaluation mode run: \n\t hmm.py -v --train TRAINING_FILE.txt --eval EVALUATION_FILE.txt \nTo run hmm.py in testing mode run: \n\t hmm.py -t --train TRAINING_FILE.txt --test TESTING_FILE.txt")
+    
+    print('\nStart Program. \n\t Execution mode: {0} \n\t trainFilePath: {1} \n\t evalOrTestFilePath: {2}'.format(executionMode, trainFilePath, evalOrTestFilePath))
+
+    return executionMode,trainFilePath,evalOrTestFilePath
+
+# Get Command Line Arguments
+executionMode,trainFilePath,evalOrTestFilePath = GetCommandLineArguments()
 
 
 print("\nHW4 - Hidden Markov model\n") 
+
